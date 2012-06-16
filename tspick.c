b4155833c15b432a842b027aee47f9b6a0db868a
Yoshihisa Tanaka <yt.hisa@gmail.com>
2012-06-16 19:04:28 +0900

    change command line arguments

--- tspick.c
+++ tspick.c
@@ -16,91 +16,118 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <dirent.h>
+#include <ctype.h>
+#include <sys/stat.h>
 
-#define true 1
-#define false 0
+#include "defs.h"
 
 #define PACKET_LENGTH 188
-#define SUFFIX "_tspick"
 
 typedef unsigned char byte;
 
-int getpid(const byte *data);
+static void usage(void);
+boolean file_exists(const char *filename);
+boolean yes_or_no(const char *msg, boolean default_);
+int get_pid(const byte *data);
 int getseclen(const byte *data);
 int find(int value, const int *array, int len);
 unsigned int crc32(const byte* data, int len);
 
 int main(int argc, char** argv) {
     FILE *in, *out;
-    DIR *dir;
-    char *infile, *outfile, *index;
     int pmt = 0, pid;
     int *pids = NULL, pidc = 0;
     byte buf[PACKET_LENGTH];
     byte z[PACKET_LENGTH - 5];
     unsigned int crc;
     int i, Nall, N;
+    int opt;
+    const char *input_filename = NULL;
+    const char *output_filename = NULL;
+    boolean force_flag = false;
+    int fd;
 
-    /* 引数を評価 */
-    if (argc == 2) {
-        /* 出力先を入力ファイルにサフィックスをつけて自動生成 */
-        infile = argv[1];
-        outfile = (char *)malloc(strlen(infile) + strlen(SUFFIX) + 1);
-        index = strrchr(infile, '.');
-        if (index != NULL) {
-            *index = '\0';
+    /* 引数の解析 */
+    while ((opt =  getopt(argc, argv, "i:o:fh")) != - 1) {
+        switch (opt) {
+        case 'i':
+            input_filename = optarg;
+            break;
+        case 'o':
+            output_filename = optarg;
+            break;
+        case 'f':
+            force_flag = true;
+            break;
+        case 'h':
+        default:
+            usage();
         }
-        strcpy(outfile, infile);
-        strcat(outfile, SUFFIX);
-        if (index != NULL) {
-            *index = '.';
-            strcat(outfile, index);
+    }
+
+    argc -= optind;
+    argv += optind;
+
+    /* 引数チェック */
+    if (input_filename != NULL && !file_exists(input_filename)) {
+        fprintf(stderr, "ERROR: input file not exists: %s\n",
+                input_filename);
+        usage();
+    }
+
+    if (output_filename != NULL && !force_flag && file_exists(output_filename)) {
+        if (input_filename == NULL) {
+            fprintf(stderr, "ERROR: output file already exists: %s\n",
+                    output_filename);
+            usage();
         }
-    } else if (argc == 3) {
-        /* 出力先指定あり */
-        infile = argv[1];
-        if ((dir = opendir(argv[2])) == NULL) {
-            /* ファイル名を指定 */
-            outfile = argv[2];
-        } else {
-            /* ディレクトリを指定された場合、入力と同じファイル名で作成する */
-            closedir(dir);
-            index = strrchr(infile, '/');
-            if (index == NULL)
-                index = infile;
-            else
-                index++;
-            if (argv[2][strlen(argv[2]) - 1] == '/')
-                argv[2][strlen(argv[2]) - 1] = '\0';
-            outfile = (char *)malloc(strlen(argv[2]) + strlen(index) + 2);
-            strcpy(outfile, argv[2]);
-            strcat(outfile, "/");
-            strcat(outfile, index);
+
+        fprintf(stderr, "output file already exists: %s\n", output_filename);
+        if (!yes_or_no("overwrite?", false))
+            usage();
+    }
+
+    /* 入力を開く */
+    if (input_filename == NULL) {
+        if ((fd = dup(fileno(stdin))) == -1 ||
+            (in = fdopen(fd, "rb")) == NULL) {
+            fprintf(stderr, "ERROR: cannot open stdin");
+            exit(EXIT_FAILURE);
         }
     } else {
-        printf("usage: tspick source.ts [dest]\n");
-        exit(EXIT_FAILURE);
+        if ((in = fopen(input_filename, "rb")) == NULL) {
+            fprintf(stderr, "ERROR: cannot open input file: %s\n", input_filename);
+            exit(EXIT_FAILURE);
+        }
     }
 
-
-    /* 抽出するデータの選定 */
-    if ((in = fopen(infile, "rb")) == NULL) {
-        printf("Can't open input file: %s\n", infile);
-        free(outfile);
-        exit(EXIT_FAILURE);
+    /* 出力を開く */
+    if (output_filename == NULL) {
+        if ((fd = dup(fileno(stdout))) == -1 ||
+            (out = fdopen(fd, "wb")) == NULL) {
+            fprintf(stderr, "ERROR: cannot open stdout");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        if ((out = fopen(output_filename, "rb")) == NULL) {
+            fprintf(stderr, "ERROR: cannot open output file: %s\n", output_filename);
+            exit(EXIT_FAILURE);
+        }
     }
 
+    /* 抽出対象を解析 */
     while (true) {
         if (fread(buf, sizeof(byte), PACKET_LENGTH, in) == 0)
             break;
 
-        pid = getpid(buf + 1);
+        pid = get_pid(buf + 1);
 
         /* PAT */
         if (pid == 0x0000) {
-            pmt = getpid(buf + 19);
+            pmt = get_pid(buf + 19);
             /* PATのPMT1のみ残した差し替えデータを作成 */
             memcpy(z, buf + 5, 16);
             /* セクション長を0x11で上書き */
@@ -128,18 +155,18 @@ int main(int argc, char** argv) {
             /* PIDを配列に格納 */
             pids[0] = pmt;
             /* PCR */
-            pids[1] = getpid(buf + 13);
+            pids[1] = get_pid(buf + 13);
             /* EPID */
             i = 2;
             for (N = getseclen(buf + 15) + 16 + 1; N < Nall + 8 - 4; N += 4 + getseclen(buf + N + 3) + 1) {
                 if (buf[N] != 0x0D)
-                    pids[i++] = getpid(buf + N + 1);
+                    pids[i++] = get_pid(buf + N + 1);
             }
 
-            printf("Extract pids");
+            fprintf(stderr, "Extract pids");
             for (i = 0; i < pidc; i++)
-                printf(", 0x%04x", pids[i]);
-            printf("\n");
+                fprintf(stderr, ", 0x%04x", pids[i]);
+            fprintf(stderr, "\n");
             break;
         }
     }
@@ -147,21 +174,11 @@ int main(int argc, char** argv) {
     /* 入力ファイルの先頭に戻す(入力をfifoにする場合はコメントアウト) */
     fseek(in, 0, SEEK_SET);
 
-    /* ファイル出力 */
-    printf("Output file: %s\n", outfile);
-    if ((out = fopen(outfile, "wb")) == NULL) {
-        printf("Can't open output file\n");
-        fclose(in);
-        free(outfile);
-        free(pids);
-        exit(EXIT_FAILURE);
-    }
-
     while (true) {
         if (fread(buf, sizeof(byte), PACKET_LENGTH, in) == 0)
             break;
 
-        pid = getpid(buf + 1);
+        pid = get_pid(buf + 1);
 
         /* PAT */
         if (pid == 0x0000) {
@@ -175,13 +192,19 @@ int main(int argc, char** argv) {
 
     fclose(out);
     fclose(in);
-    free(outfile);
     free(pids);
 
     return EXIT_SUCCESS;
 }
 
-int getpid(const byte *data) {
+void usage(void) {
+    fprintf(stderr,
+            "usage: tspick [-h] [-i INPUT_FILE] [-o OUTPUT_FILE]\n"
+            );
+    exit(EXIT_FAILURE);
+}
+
+int get_pid(const byte *data) {
     return ((data[0] & 0x1F) << 8) + data[1];
 }
 
@@ -220,4 +243,33 @@ unsigned int crc32(const byte *data, int len) {
         }
     }
     return crc;
+}
+
+int file_exists(const char *filename) {
+    struct stat sb;
+    return stat(filename, &sb) == 0 ? true : false;
+}
+
+boolean yes_or_no(const char *msg, boolean default_) {
+    char buf[5];
+    char ans;
+
+    while (1) {
+        fprintf(stderr, "%s (%s)\n", msg, default_ ? "Y/n" : "y/N");
+        if (fgets(buf, sizeof(buf), stdin) == NULL)
+            return false;
+
+        if (strlen(buf) == 1)
+            return default_;
+
+        if (strlen(buf) == 2) {
+            ans = tolower(buf[0]);
+            if (ans == 'y')
+                return true;
+            if (ans == 'n')
+                return false;
+        }
+
+        fflush(stdin);
+    }
 }
